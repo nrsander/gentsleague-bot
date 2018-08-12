327cda8876a0dc06cc6bfc147929e489f79eda9a
nrsander <nrsander@gmail.com>
2018-08-11 23:52:27 -0400

    fix

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ client.on("message", (message) => {
 });
 
 // login to Discord with your app's token
-client.login('NDc3NjIxNzQ2MDMyNDQzMzk0.Dk-z7g.tR8sVcWW2qgKTIKP4s_DoLK1J5s');
+client.login('NDc3NjIxNzQ2MDMyNDQzMzk0.DlFASw.Sws8lh0YPDX2TbxtGl4BP98T9LI');
