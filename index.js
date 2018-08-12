025fc4a3f4f02070f6956f17f9fa0588cc185678
nrsander <nrsander@gmail.com>
2018-08-12 16:04:50 -0400

    debug

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 // STARTUP:   //
 //============//
 
+console.log('||  <---------------------  Test Boot ...');
+
 const express = require("express");
+console.log('Express loaded');
 const server = express();
+console.log('Express server started successfully');
 const PORT = process.env.PORT;
 console.log(PORT);
+console.log('Server about to start listening ...');
 server.listen(PORT);
 
 
@@ -13,9 +18,12 @@ server.listen(PORT);
 // cd Desktop/CRYPTO\ LYFE/Checkup/GLBot
 
 const Discord = require('discord.js'); // Require the discord.js module
+console.log('Discord.js loaded');
 const client = new Discord.Client(); // Create a new Discord client
+console.log('Discord client started successfully');
 const config = require("./config.json");
 const token = process.env.GL_DISCORD_TOKEN;
+console.log('Hid the Russian bot token in a secret place');
 
 console.log('Firing up GLBot  ...');
 
@@ -32,16 +40,18 @@ client.on('ready', function() {
 //============//
 
 client.on("message", (message) => {
-
+  console.log('debug 0');
   // (integrity checks)
   if(message.author.bot) return;
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
   // (msg->lowercase)
   msgCntnt = map(str.lower, message.content);
+  console.log('debug 1');
 
   // Collusion-detector
   if (msgCntnt.includes("collusion")) {
+    console.log('debug 2');
     message.channel.send("COLLUSION!!!??????");
   }
 });
