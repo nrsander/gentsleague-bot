e3ec1c0cd4e05db1e4e9aa11bb68d783cf5e6e96
nrsander <nrsander@gmail.com>
2018-08-11 23:08:55 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// cd Desktop/CRYPTO\ LYFE/Checkup/GLBot
+
+
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will trigger whenever your bot:
+// - finishes logging in
+// - reconnects after disconnecting
+client.on('ready', function() {
+    console.log('Ready!')//;
+});
+
+// login to Discord with your app's token
+client.login('NDc3NjIxNzQ2MDMyNDQzMzk0.Dk-z7g.tR8sVcWW2qgKTIKP4s_DoLK1J5s');
