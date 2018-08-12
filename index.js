5605c89e694f5f6188bfc208318b218333b25458
nrsander <nrsander@gmail.com>
2018-08-12 11:31:21 -0400

    index

--- index.js
+++ index.js
@@ -39,23 +39,6 @@ client.on("message", (message) => {
   }
 });
 
-//client.on("message", message => {
-//  if (message.author.bot) return;
-//  if (message.content.indexOf(config.prefix) !== 0) return;
-​//
-//  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-//  const command = args.shift().toLowerCase();
-​//
-//  if(command === 'collusion') {
-//    message.channel.send('COLLUSION!!!!???????!?!');
-//  } else
-//  if (command === '9') {
-//    message.channel.send('Meh.');
-//  }
-//});
-
-
-
 
 //============//
 // INIT BOT:  //
