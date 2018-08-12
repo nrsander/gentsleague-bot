3f521e7d55f2d1b500ae4beaa65fe2ec27aba0b5
nrsander <nrsander@gmail.com>
2018-08-12 11:46:49 -0400

    gvhb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //============//
 
 var port = process.env.PORT || 8000;
+let server = require('http').Server(app);
 server.listen(port, function() {
     console.log("GLApp is running on port " + port);
 });
