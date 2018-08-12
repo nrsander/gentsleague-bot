7eefadbcaed54a7d35ad7fffbd9e1d4ae4133aed
nrsander <nrsander@gmail.com>
2018-08-12 09:49:38 -0400

    config add

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
+// (If running locally:)
 // cd Desktop/CRYPTO\ LYFE/Checkup/GLBot
 
 
-// require the discord.js module
-const Discord = require('discord.js');
+//============//
+// SETUP:     //
+//============//
 
-// create a new Discord client
-const client = new Discord.Client();
+const Discord = require('discord.js'); // Require the discord.js module
+const client = new Discord.Client(); // Create a new Discord client
+const config = require("./config.json");
 
-// when the client is ready, run this code
-// this event will trigger whenever your bot:
-// - finishes logging in
-// - reconnects after disconnecting
+console.log('Firing up GLBot ...')
+
 client.on('ready', function() {
-    console.log('Firing up GLBot ...')//;
+    console.log('GLBot deployed.')//;
 });
+// ^ This event runs each time GLBot ...
+//     - Finishes logging in
+//     - Reconnects after disconnecting
+
+
+//============//
+// COMMANDS:  //
+//============//
 
 client.on("message", (message) => {
-  if (message.content.includes("collusion")) {
-    message.channel.send("COLLUSION!!!!!????????");
-  }
+
+  // (integrity checks)
+  if(message.author.bot) return;
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  // (msg->lowercase)
+  msgCntnt = map(str.lower, message.content)
+
+  // Collusion-detector
+  if (msgCntnt.includes("collusion")) {
+    message.channel.send("COLLUSION!!!??????");
+  } else
+​  // Power-rankings mention detector
+  //if (msgCntnt.includes("power rankings")) {
+  //  message.channel.send("Did somebody say Power Rankings??");
+  //}
 });
 
+//client.on("message", message => {
+//  if (message.author.bot) return;
+//  if (message.content.indexOf(config.prefix) !== 0) return;
+​//
+//  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+//  const command = args.shift().toLowerCase();
+​//
+//  if(command === 'collusion') {
+//    message.channel.send('COLLUSION!!!!???????!?!');
+//  } else
+//  if (command === '9') {
+//    message.channel.send('Meh.');
+//  }
+//});
+
+
+
+
+//============//
+// BOT LOGIN: //
+//============//
+
 // login to Discord with your app's token
-client.login('NDc3NjIxNzQ2MDMyNDQzMzk0.DlFASw.Sws8lh0YPDX2TbxtGl4BP98T9LI');
+client.login(config.token);
