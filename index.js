d50ca0e3ab2ba9a967b028cc29022bb66f74bb61
nrsander <nrsander@gmail.com>
2018-08-17 22:26:33 -0400

    fixeruppers

--- index.js
+++ index.js
@@ -2,115 +2,147 @@
 // STARTUP:   //
 //============//
 
-console.log('||  <---------------------  Booting GL Bot ...');
+// (config)
+const config = require("./config.json");
+// (env vars)
+const token = process.env.GL_DISCORD_TOKEN;
+console.log('Hid the Russian bot token in a secret place.');
 
+// ---------------------------------
+
+// Set up HTTP server
+console.log('||  <---------------------  Booting GL Bot ...');
 const express = require("express");
 const server = express();
 const PORT = process.env.PORT;
-console.log(PORT);
-console.log('Server about to start listening ...');
+console.log('Server about to start listening on port ' + PORT + ' ...');
 server.listen(PORT);
+console.log('Listening.');
 
+// ESPN Fantasy connection
 const espnFF = require('espn-ff-api');
 const cookies = {
   espnS2 : 'AECEQQFfq9v%2FKF46rl7BoFgHimTDQb5YtXB1lwH6kDf7cRKjMsTUunVQaZTZjsCs7SOLB5L1Y2b7mMDPH7Hgblhb7KUB2assXYlXNRKnwU3DzeN7KRF8zw8EFYX3FFhAmyp4N%2Fl%2BseRM25dqnqI44auvCWrlW9Q0P4DeTDGH%2F9o4BCQXPxnUcWjnIk%2Ff6y8PGBDUPOAPRVoE25CwKaH6bO4ELgI216Xev8wl004HIohqh%2FAs3Xq69S2t3XdUdZTXKAU%3D',
   SWID   : '{D03EEEB0-C999-42DA-9394-C054A93119A9}'
 };
-
-// R-script
-//var R = require("r-script"); // (Removed from package.json)
+const leagueId = 175917;
 
 
-//
-//
-//
+// Example file system  (test)
 var fs = require("fs");
 var data = fs.readFileSync('example.txt');
 console.log(data.toString());
 console.log("(.) -- Example Displayed -- (.)");
-//
-//
-//
 
-const leagueId = 175917;
 
-// (If running locally:)
-// cd Desktop/CRYPTO\ LYFE/Checkup/GLBot
-
+// Set up Discord client
 const Discord = require('discord.js'); // Require the discord.js module
 const client = new Discord.Client(); // Create a new Discord client
 console.log('Discord client started successfully');
-const config = require("./config.json");
-const token = process.env.GL_DISCORD_TOKEN;
-console.log('Hid the Russian bot token in a secret place');
 
-console.log('Firing up RoboCommish  ...');
 
-client.on('ready', function() {
-    console.log('RoboCommish successfully deployed.')
-});
-// ^ This event runs each time GLBot ...
-//     - Finishes logging in
-//     - Reconnects after disconnecting
+//R-script
+//var R = require("r-script"); // (Removed from package.json)
+
+
+// ---------------------------------
+
+
+
+
+//============//
+// PARAMS:    //
+//============//
+
+const week = 16;
+const playersLimit = 1;
+
+
+// ---------------------------------
+
+
 
 
 //============//
 // COMMANDS:  //
 //============//
 
+// Start (/restart) Discord client
+client.on('ready', function() {
+  console.log('');
+  console.log('Firing up RoboCommish  ...');
+  console.log('');
+  console.log('RoboCommish successfully deployed.')
+});
+
+
+// Message received
 client.on("message", (message) => {
 
-  // (integrity checks)
+  // (Ensure this msg is NOT from a bot/self ( => infinite loop error))
   if (message.author.bot) {
-    return;
+    return; // quit
   }
+  // (This msg is actually a command)
   if (message.content.startsWith(config.prefix)) {
-    return;
+    return; // quit
   }
 
+  // convert msg to lowercase
   msgCntnt = message.content.toString().toLowerCase();
 
 
-  // ESPN-getter
+  // if msg = {my makeshift 'scrape ESPN league' command} then ...
   if (msgCntnt.includes("^teams")) {
-
     console.log('[ ----- Scraping data from the Gentleman\'s League ESPN page ... ----- ]');
+
     message.author.send("Thinking ...");
 
-    var week = 16;
-    var playersLimit = 1;
-
-    // Returns simplified league object
+    // Get Overall Standings
+    //    returns simplified league object
     espnFF.getOverallStandings(cookies, leagueId)
           .then(result => {
             var resultSize = result.length;
-            console.log("ESPN --> GL --> getOverallStandings:");
+
+            console.log("Result:");
             console.log(result);
+            console.log("  ^  =  ESPN-->GL-->getOverallStandings(...)");
+            console.log("Result size = " + resultSize);
 
-            message.channel.send("Week " + week + " Starting QBs:");
+            message.channel.send("QB1 - Week " + week);
 
-            var i;
             var tmid;
+            var i;
             for (i = 0; i <= resultSize - 1; i++) {
 
-              // Main response:
-              message.channel.send(result[i].teamLocation + " " + result[i].teamNickname + "\t(GL #" + result[i].teamId + ")\n\tWins: " + result[i].wins + "\tPts: " + result[i].pointsFor);
-              console.log(result[i].teamLocation + " " + result[i].teamNickname + "\t(GL #" + result[i].teamId + ")\n\tWins: " + result[i].wins + "\tPts: " + result[i].pointsFor);
+              // Bot Response:
+              message.channel.send(result[i].teamLocation + " " + result[i].teamNickname + " [GL #" + result[i].teamId + "] " + result[i].wins + " wins, " + result[i].pointsFor + " pts.");
+              console.log(result[i].teamLocation + " " + result[i].teamNickname + " [GL #" + result[i].teamId + "] " + result[i].wins + " wins, " + result[i].pointsFor + " pts.");
 
-              // Get QB1's:
+              // Get QB1's ...
+
+              // Which team are we on?
               tmid = result[i].teamId;
+              console.log("Team ID   : " + tmid);
+              console.log("Team Name : " + result[i].teamLocation + " " + result[i].teamNickname);
+
               espnFF.getSingleTeamPlayers(cookies, leagueId, tmid, week)
                     .then(players => {
+                      console.log("Players: " + players);
+                    })
+                    .done(result => {
                       rstr = '';
                       for (j=0; j<=playersLimit-1; j++) {
                         if(j>0){
                           rstr += ", "
                         };
                         rstr += players[j].playerName;
-                        console.log("TmId-PlayerNo:  " + tmid + "-" + j);
+                        console.log("(" + j + ")" + "Player    : " + players[j].playerName);
                       }
-                      console.log("Players:\t" + rstr);
-                      message.channel.send(rstr);
+                      console.log("All " + playersLimit + " Requested Players:\n\t" + rstr);
+                      message.channel.send("Req'd Players:\n" + rstr);
+
+                      console.log('==> GetSingleTeamPlayers ==> Complete (100%)');
                     });
 
             }
